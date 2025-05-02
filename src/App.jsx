817fbed9c3fca2cc7ebc41c8322396541fc085e4
Swetha nal <n.swetha597@gmail.com>
2025-05-01 23:46:34 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import './App.css'
+import Mainpage from './shoppingfolder/pages/Mainpage'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Shopping Mall</h1>
+      <Mainpage />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/shoppingfolder/pages/Mainpage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Collections from '../components/Collections'
+
+const Mainpage = () => {
+  return (
+    <div>
+       <Header />
+       <Banner />
+       <Collections />
+        <Footer />
+
+    </div>
+  )
+}
+
+export default Mainpage
